6851a173ae234dcf81655eb379219fbd4de8643d
Teten Nugraha <Teten.nugraha18@gmail.com>
2023-05-12 09:53:32 +0700

    first launch

---
+++ src/main/java/id/ten/springimportcsvpostgres/SpringImportCsvPostgresApplication.java
@@ -0,0 +1,13 @@
+package id.ten.springimportcsvpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringImportCsvPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringImportCsvPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ten/springimportcsvpostgres/SpringImportCsvPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.springimportcsvpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringImportCsvPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
