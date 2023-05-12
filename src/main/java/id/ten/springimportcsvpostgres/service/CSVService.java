af36c20936d0e728be354089adfaefa419316108
Teten Nugraha <Teten.nugraha18@gmail.com>
2023-05-12 10:22:29 +0700

    feat : create CSV Service for upload

---
+++ src/main/java/id/ten/springimportcsvpostgres/service/CSVService.java
@@ -0,0 +1,28 @@
+package id.ten.springimportcsvpostgres.service;
+
+import id.ten.springimportcsvpostgres.helpers.CSVHelper;
+import id.ten.springimportcsvpostgres.models.Mahasiswa;
+import id.ten.springimportcsvpostgres.repository.MahasiswaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CSVService {
+
+    private MahasiswaRepository mahasiswaRepository;
+
+    public void save(MultipartFile file) {
+        try {
+            final List<Mahasiswa> mahasiswaList = CSVHelper.transformCSVToList(file.getInputStream());
+            mahasiswaRepository.saveAll(mahasiswaList);
+        }catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+}
